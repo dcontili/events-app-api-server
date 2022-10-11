141b651f44e840b2da214c0b1761241573a8b356
dcontili <dcontili@gmail.com>
2022-10-11 16:24:30 +0000

    Updated team name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Team 3';
+    team = 'Team 3 New Name';
     version = '1.0.1';
 }
 module.exports = AppConfig;
